502be68034eadd21ee1955f5d1630587b38cc5b9
Uriel Guy <urielsateles@gmail.com>
2023-03-08 23:27:34 -0300

    fix: add photo test

--- src/apollo/src/modules/property/resources/__tests__/property.resource.spec.ts
+++ src/apollo/src/modules/property/resources/__tests__/property.resource.spec.ts
@@ -1,14 +1,21 @@
 import Context from '../../../../context';
 import { ApolloTestContext } from '../../../shared/tests/apollo-test-helper';
 import { Upload } from 'graphql-upload';
-import { Readable } from 'stream';
+import { createReadStream, readdirSync } from 'fs';
+import { ReadStream } from 'fs-capacitor';
 describe('PropertyResource', () => {
   let client: any;
   let ctx: ApolloTestContext;
   let context: Context;
 
   beforeEach(async () => {
-    context = new Context().getTestContext();
+    context = await new Context().getContext({
+      req_id: 'test',
+      req_path: 'test',
+      req_method: 'test',
+      req_ua: 'test',
+      headers: {},
+    });
     ctx = new ApolloTestContext('int', context);
     client = await ctx.makeClient();
   });
@@ -61,18 +68,30 @@ describe('PropertyResource', () => {
   });
 
   it('should create property', async () => {
-    const upload = new Upload();
-    const file = Readable.from(
-      Buffer.from(`${__dirname}/int-apollo-upload-test.txt`, 'utf8'),
-    ) as any;
-    upload.promise = new Promise((resolve) =>
-      resolve({
-        filename: 'int-apollo-upload-test.txt',
-        mimetype: 'text/plain',
-        encoding: '7bit',
-        createReadStream: () => file,
-      }),
-    );
+    const uploads = [] as Upload[];
+    readdirSync(`${__dirname}/photos/`).forEach((filename) => {
+      const file: any = createReadStream(`${__dirname}/photos/${filename}`);
+      const upload = new Upload();
+      upload.promise = new Promise((resolve) =>
+        resolve({
+          filename,
+          mimetype: 'image/jpeg',
+          encoding: '7bit',
+          createReadStream: () => file,
+        }),
+      ); //`${__dirname}/photos/${filename}`
+      uploads.push(upload);
+    });
+    context.authService = {
+      authenticate: jest.fn(),
+      authorize: jest.fn(),
+    } as any;
+    context.user = {
+      id: 'MgxO159FtDCCYQYULEhBy',
+      role: {
+        name: 'admin',
+      },
+    };
     const res = await client.executeOperation(
       {
         query: `mutation CreateProperty($input: create_property_input!) {
@@ -83,7 +102,7 @@ describe('PropertyResource', () => {
         variables: {
           input: {
             ...makeInput(),
-            photos: [upload],
+            photos: uploads,
           },
         },
       },
--- src/core/src/shared/domain/value-objects/__tests__/value-object.spec.ts
+++ src/core/src/shared/domain/value-objects/__tests__/value-object.spec.ts
@@ -17,6 +17,8 @@ describe('ValueObject Unit Tests', () => {
     const date = new Date();
     const arrange = [
       { received: '', expected: '' },
+      { received: null, expected: 'null' },
+      { received: undefined, expected: 'test,test2' },
       { received: 'fake test', expected: 'fake test' },
       { received: 0, expected: '0' },
       { received: 1, expected: '1' },
@@ -24,15 +26,24 @@ describe('ValueObject Unit Tests', () => {
       { received: true, expected: 'true' },
       { received: false, expected: 'false' },
       { received: date, expected: date.toString() },
+      { received: ['test', 'test2'], expected: 'test,test2' },
       {
         received: { prop1: 'value1' },
         expected: JSON.stringify({ prop1: 'value1' }),
+      },
+      { received: () => 'function', expected: "()=>'function'" },
+      {
+        received: function () {
+          return 'function';
+        },
+        expected:
+          "function() {\n                    return 'function';\n                }",
       },
     ];
 
     arrange.forEach((value) => {
       const vo = new StubValueObject(value.received);
-      expect(vo + '').toBe(value.expected);
+      expect(vo.toString()).toBe(value.expected);
     });
   });
 });
