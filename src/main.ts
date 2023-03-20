12ea3329c71bf630669ef44f6294f08b4ddbed17
Uriel Guy <urielsateles@gmail.com>
2023-03-20 10:20:51 -0300

    fix: handler function

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,14 @@ import NexusSchema from './apollo/schema';
 import ApolloLambdaServer from './apollo/server/apollo-lambda.server';
 import { Context } from './apollo/context';
 
-const schema = NexusSchema.makeSchema();
-const context = new Context();
-
 export const handler = async (
   event: APIGatewayEvent,
   ctx: AwsContext,
   callback: Callback<any>,
 ) => {
   console.log(__dirname);
+  const schema = NexusSchema.makeSchema();
+  const context = new Context();
   const server = new ApolloLambdaServer(schema, context);
   const handler = await server.listen();
   return handler(
