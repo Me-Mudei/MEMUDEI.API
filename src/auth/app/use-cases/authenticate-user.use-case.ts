38eb7210139ea3ca834c34d0f6bdd7444c0d617e
Uriel Guy <urielsateles@gmail.com>
2023-07-28 16:44:33 -0300

    fix: refactor auth service

--- src/auth/app/use-cases/authenticate-user.use-case.ts
+++ src/auth/app/use-cases/authenticate-user.use-case.ts
@@ -1,7 +1,7 @@
 import { UseCase } from "#shared/app";
 
 import { UserRepository } from "../../domain/repository";
-import { AuthGateway } from "../../infra";
+import { AuthGateway, Session } from "../../infra";
 import { AuthenticateUserInput, AuthenticateUserOutput } from "../dto";
 
 export class AuthenticateUserUseCase
@@ -16,8 +16,15 @@ export class AuthenticateUserUseCase
 
   async execute(input: AuthenticateUserInput): Promise<AuthenticateUserOutput> {
     console.log("START:AuthenticateUserUseCase");
-    const authenticate = await this._authGateway.decodeToken(input.token);
-    console.log("authenticate", authenticate);
+    let authenticate: Session;
+    this._authGateway.decodeToken(input.token, (err, decoded) => {
+      console.log("START:AuthenticateUserUseCase.decodeToken");
+      console.log("err", err);
+      console.log("decoded", decoded);
+      authenticate = decoded;
+      console.log("END:AuthenticateUserUseCase.decodeToken");
+    });
+    if (!authenticate) throw new Error("Invalid token");
     const user = await this._userRepository.findByExternalId(authenticate.sub);
     console.log("user", user);
     console.log("END:AuthenticateUserUseCase");
--- src/auth/infra/auth-gateway/auth-gateway.ts
+++ src/auth/infra/auth-gateway/auth-gateway.ts
@@ -15,7 +15,6 @@ export type Session = {
 export class AuthGateway {
   private getKey(header: any, callback: any) {
     console.log("START:AuthGateway.getKey");
-    console.log(configEnv.auth.domain);
     const client = jwksClient({
       cache: true,
       cacheMaxAge: 86400000,
@@ -23,9 +22,6 @@ export class AuthGateway {
       jwksRequestsPerMinute: 10,
       jwksUri: `https://${configEnv.auth.domain}/.well-known/jwks.json`
     });
-    console.log("header", header);
-    console.log("client", client);
-    console.log("getSigningKey", client.getSigningKey.toString());
     client.getSigningKey(header.kid, function (error, key) {
       console.log("START:AuthGateway.getKey.getSigningKey");
       console.log("error", error);
@@ -39,34 +35,17 @@ export class AuthGateway {
     });
   }
 
-  async decodeToken(token: string) {
+  decodeToken(token: string, callback?: (err: any, decoded?: Session) => void) {
     console.log("START:AuthGateway");
-    console.log(token.replace("Bearer ", ""), this.getKey, {
-      audience: configEnv.auth.audience,
-      issuer: `https://${configEnv.auth.domain}/`,
-      algorithms: ["RS256"]
-    });
-    return new Promise<Session>((resolve, reject) => {
-      verify(
-        token.replace("Bearer ", ""),
-        this.getKey,
-        {
-          audience: configEnv.auth.audience,
-          issuer: `https://${configEnv.auth.domain}/`,
-          algorithms: ["RS256"]
-        },
-        (err, decoded: Session) => {
-          console.log("START:AuthGateway.decodeToken");
-          console.log("err", err);
-          console.log("decoded", decoded);
-          if (err) {
-            return reject(
-              new Error(`Failed to authenticate token: ${err.message}`)
-            );
-          }
-          resolve(decoded);
-        }
-      );
-    });
+    verify(
+      token.replace("Bearer ", ""),
+      this.getKey,
+      {
+        audience: configEnv.auth.audience,
+        issuer: `https://${configEnv.auth.domain}/`,
+        algorithms: ["RS256"]
+      },
+      callback
+    );
   }
 }
