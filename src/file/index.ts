85771cbdc166737cd275b72337b863255dd70299
Uriel Guy <urielsateles@gmail.com>
2023-07-30 21:03:55 -0300

    fix: remove regressions

--- src/file/index.ts
+++ src/file/index.ts
@@ -7,58 +7,29 @@ const fileFacadeFactory = FileFacadeFactory.create();
 const authService = AuthFacadeFactory.create();
 
 export const handler = async (event: APIGatewayEvent, _ctx: Context) => {
-  try {
-    await BusboyUploadProcessor.parser(event as any);
-    const body = event.body as BusboyBody;
-    //const token =
-    //  event.headers["authorization"] || event.headers["Authorization"];
-    //const { permissions } = await authService.authenticate({ token });
-    //if (!permissions.includes(`upload:${body.type}`)) {
-    //  throw new UnauthorizedError();
-    //}
-    const output = await fileFacadeFactory.uploadFile({
-      reference_type: body.type,
-      files: body.files.map(({ file, filename, mimetype }) => ({
-        filename: filename,
-        mimetype: mimetype,
-        createReadStream: () => file
-      }))
-    });
-    return {
-      statusCode: 200,
-      body: JSON.stringify(output),
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Credentials": true,
-        "content-type": "application/json"
-      }
-    };
-  } catch (error) {
-    console.log(error);
-    console.log(JSON.stringify(error, null, 2));
-    const statusCode = {
-      BadRequestError: 400,
-      UnauthorizedError: 401,
-      NotFoundError: 404,
-      ValidationError: 422,
-      InternalServerError: 500
-    };
-    const errorBody = {
-      errors: [
-        {
-          type: error.name || "InternalServerError",
-          message: error.message
-        }
-      ]
-    };
-    return {
-      statusCode: statusCode[error.name] || 500,
-      body: JSON.stringify(errorBody),
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Credentials": true,
-        "content-type": "application/json"
-      }
-    };
-  }
+  await BusboyUploadProcessor.parser(event as any);
+  const body = event.body as BusboyBody;
+  //const token =
+  //  event.headers["authorization"] || event.headers["Authorization"];
+  //const { permissions } = await authService.authenticate({ token });
+  //if (!permissions.includes(`upload:${body.type}`)) {
+  //  throw new UnauthorizedError();
+  //}
+  const output = await fileFacadeFactory.uploadFile({
+    reference_type: body.type,
+    files: body.files.map(({ file, filename, mimetype }) => ({
+      filename: filename,
+      mimetype: mimetype,
+      createReadStream: () => file
+    }))
+  });
+  return {
+    statusCode: 200,
+    body: JSON.stringify(output),
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true,
+      "content-type": "application/json"
+    }
+  };
 };
