72c3efcc308efad81b3088a43424e1d22cbc630a
Uriel Guy <urielsateles@gmail.com>
2023-08-05 17:42:09 -0300

    fix: files migration

--- prisma/migrations/20230805204144_files_change/migration.sql
+++ prisma/migrations/20230805204144_files_change/migration.sql
@@ -2,7 +2,6 @@
   Warnings:
 
   - You are about to drop the column `description` on the `file` table. All the data in the column will be lost.
-  - Added the required column `position` to the `properties_files` table without a default value. This is not possible if the table is not empty.
 
 */
 -- AlterTable
@@ -10,4 +9,4 @@ ALTER TABLE "file" DROP COLUMN "description";
 
 -- AlterTable
 ALTER TABLE "properties_files" ADD COLUMN     "description" TEXT,
-ADD COLUMN     "position" INTEGER NOT NULL;
+ADD COLUMN     "position" INTEGER;
