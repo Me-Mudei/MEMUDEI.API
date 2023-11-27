3330868d23544b13250642c24b053ad156360ce0
Uriel Guy <urielsateles@gmail.com>
2023-11-27 13:21:43 -0300

    fix: remove unused deps and envs

--- src/shared/infra/config/env.config.ts
+++ src/shared/infra/config/env.config.ts
@@ -1,5 +1,8 @@
 type Config = {
   env: string;
+  database: {
+    url: string;
+  };
   log: {
     level: string;
   };
@@ -10,17 +13,6 @@ type Config = {
     domain: string;
     client_id: string;
   };
-  cloud: {
-    vendor: "AWS" | "LOCALSTACK";
-    accessKeyId: string;
-    secretAccessKey: string;
-    region: string;
-    endpoint: string;
-  };
-  storage: {
-    vendor: "S3";
-    bucket: string;
-  };
   crm: {
     vendor: "HUBSPOT";
     accessToken: string;
@@ -30,6 +22,9 @@ type Config = {
 export function makeConfig(): Config {
   return {
     env: process.env.NODE_ENV,
+    database: {
+      url: process.env.DATABASE_URL
+    },
     log: {
       level: process.env.LOG_LEVEL
     },
@@ -39,17 +34,6 @@ export function makeConfig(): Config {
       issuer: process.env.AUTH_ISSUER,
       domain: process.env.AUTH_DOMAIN,
       client_id: process.env.AUTH_CLIENT_ID
-    },
-    cloud: {
-      vendor: process.env.CLOUD_VENDOR as any,
-      accessKeyId: process.env.CLOUD_ACCESS_KEY_ID,
-      secretAccessKey: process.env.CLOUD_SECRET_ACCESS_KEY,
-      region: process.env.CLOUD_REGION,
-      endpoint: process.env.CLOUD_ENDPOINT
-    },
-    storage: {
-      vendor: process.env.STORAGE_VENDOR as any,
-      bucket: process.env.STORAGE_BUCKET
     },
     crm: {
       vendor: process.env.CRM_VENDOR as any,
